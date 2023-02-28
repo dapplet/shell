1d670c230cf0b8716cf292ef0e090adc882f6265
tjvsx <vanslotj@gmail.com>
2023-02-27 23:46:30 -0500

    ci: 3

--- src/lib/constants/production.ts
+++ src/lib/constants/production.ts
@@ -1,16 +1,12 @@
-import { Config, Goerli, Mainnet, Sepolia } from '@usedapp/core';
+import { Config, Sepolia } from '@usedapp/core';
 import { deployment } from '../../contracts';
 
 const production: Config = {
   readOnlyChainId: Sepolia.chainId,
   readOnlyUrls: {
-    [Mainnet.chainId]: process.env.REACT_APP_INFURA_ETH_MAINNET_URL!,
-    [Goerli.chainId]: process.env.REACT_APP_INFURA_ETH_GOERLI_URL!,
     [Sepolia.chainId]: process.env.REACT_APP_INFURA_ETH_SEPOLIA_URL!,
   },
   multicallAddresses: {
-    [Mainnet.chainId]: deployment('Multicall2', Mainnet.chainId)?.address,
-    [Goerli.chainId]: deployment('Multicall2', Goerli.chainId)?.address,
     [Sepolia.chainId]: deployment('Multicall2', Sepolia.chainId)?.address,
   },
   multicallVersion: 2,
